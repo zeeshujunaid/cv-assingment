5e07868514606640f50b2bfa1daef44acabfdc94
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-06-02 02:50:40 +0500

    almost done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import profilePicture from "./images/profile.jpg"; // Replace with your profile picture path
+import profilePicture from "./zeeshan-removebg-preview.png"; // Replace with your profile picture path
 
 function App() {
   return (
@@ -9,53 +9,57 @@ function App() {
         <div className="profile-picture">
           <img src={profilePicture} alt="Profile" />
         </div>
-        <h1>Hasan</h1>
-        <h2>Frontend Developer</h2>
+        <h1>zeeshan junaid</h1>
+        <h2>Fullstack Developer</h2>
         <div className="section">
-          <h3>Skills</h3>
+          <h3>LANGUAGES & FRAMEWORK</h3>
           <ul>
             <li>HTML</li>
             <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
+            <li>JAVA SCRIPT</li>
+            <li>TYPE SCRIPT</li>
+            <li>REACT NATIVE</li>
+            <li>REACT</li>
+            <li>BOOT STRAP</li>
           </ul>
         </div>
         <div className="section">
-          <h3>Contact</h3>
+          <h3>CONTACT</h3>
           <ul>
-            <li>033333333333</li>
-            <li>smit@gmail.com</li>
+            <li>03300342644</li>
+            <li>zeeshan.junaid.Developer@gmail.com</li>
             <li>Karachi Pakistan</li>
           </ul>
         </div>
         <div className="section">
-          <h3>Hobbies & Interest</h3>
+          <h3>HOBIES & INTEREST</h3>
           <ul>
-            <li>Traveling</li>
-            <li>Driving</li>
-            <li>Gaming</li>
-            <li>paragliding</li>
+            <li>TRAVELING</li>
+            <li>PHOTOGRAPHY</li>
+            <li>CRICKET</li>
+            <li>DRIVING</li>
+            <li>GAMING</li>
+            <li>PARAGLIDING</li>
           </ul>
         </div>
 
         <div className="section">
-          <h3>Honor & Award</h3>
-          <p>
-            <p className="y">2024</p>
-           lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-            aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
-          </p>
+
         </div>
       </div>
       <div className="right-column">
         <div className="section">
-          <h3 className="bg-color">Education</h3>
+          <h3 className="bg-color">EDUCATION</h3>
           <div className="item">
-            <h4>2022-2024</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-              aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
-            </p>
+            <h4>
+            CIVIC SCHOOL
+            </h4>
+            <h4>
+            GOVERNER IT INITIATIVE
+            </h4>  
+            <h4>
+            SAYLANI MASS IT TRAINING CENTER
+            </h4>
           </div>
         </div>
         <div className="section">
@@ -68,16 +72,16 @@ function App() {
             </p>
           </div>
           <div className="item">
-            <h4>2022-2024</h4>
             <h5 className="bg-color">Name of Certification</h5>
+            <h4>2022-2024</h4>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
               aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
             </p>
           </div>
           <div className="item">
-            <h4>2022-2024</h4>
             <h5 className="bg-color">Name of Certification</h5>
+            <h4>2022-2024</h4>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
               aliquam scelerisque aliquam. Nulla egestas pulvinar arcu.
@@ -87,10 +91,7 @@ function App() {
         <div className="section">
           <h3 className="bg-color">Languages</h3>
           <ul>
-            <li>
-             
-              English - 60%
-            </li>
+            <li>English - 60%</li>
             <li>Urdu - 75%</li>
           </ul>
         </div>
